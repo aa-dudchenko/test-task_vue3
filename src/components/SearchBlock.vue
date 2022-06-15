b025189cf98866353ee51f091ad90a01f64b04cf
DudchenkoWeb <aa.dudchenko00@gmail.com>
2022-06-15 17:22:33 +0300

    before pagination

--- src/components/SearchBlock.vue
+++ src/components/SearchBlock.vue
@@ -2,7 +2,11 @@
   <div class="search-block">
     <div class="container search-block__container">
 
-      <form class="search-block__form" action="">
+      <form
+          class="search-block__form" action=""
+          @click = "openSearch"
+          :class="{ 'search-block__form_active': isActive }"
+      >
         <input class="search-block__input" type="search">
         <svg class="search-block__icon" enable-background="new 0 0 26 32" id="Glyph" version="1.1" viewBox="0 0 26 32" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M27.414,24.586l-5.077-5.077C23.386,17.928,24,16.035,24,14c0-5.514-4.486-10-10-10S4,8.486,4,14  s4.486,10,10,10c2.035,0,3.928-0.614,5.509-1.663l5.077,5.077c0.78,0.781,2.048,0.781,2.828,0  C28.195,26.633,28.195,25.367,27.414,24.586z M7,14c0-3.86,3.14-7,7-7s7,3.14,7,7s-3.14,7-7,7S7,17.86,7,14z" id="XMLID_223_"/></svg>
       </form>
@@ -15,6 +19,17 @@
 export default {
   name: 'SearchBlock',
 
+  data () {
+    return {
+      isActive: false,
+    }
+  },
+
+  methods: {
+    openSearch () {
+      this.isActive = !this.isActive
+    }
+  }
 }
 </script>
 
@@ -41,16 +56,19 @@ export default {
       border: 4px solid $clr-white;
       padding: 5px;
 
-      &:hover{
+      &:hover,
+      &:active {
         width: 250px;
         cursor: pointer;
       }
 
-      &:hover .search-block__input {
+      &:hover .search-block__input,
+      &:active .search-block__input {
          display: block;
       }
 
-      &:hover .search-block__icon{
+      &:hover .search-block__icon,
+      &:active .search-block__icon {
         background: $clr-dark;
         color: $clr-white;
         fill: $clr-white;
